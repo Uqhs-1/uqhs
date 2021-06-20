14df8ff92fb0b3a08d7c97e86c06c20d8827c913
Uqhs-1 <uqhs2019@gmail.com>
2021-06-20 20:51:12 +0100

    celery_push

--- result/tasks.py
+++ result/tasks.py
@@ -1,16 +1,28 @@
 # in any app that you want celery tasks, make a tasks.py and the celery app will autodiscover that file and those tasks.
 from __future__ import absolute_import
 from celery import shared_task
+from django.contrib.auth.models import User
+from .utils import session, Render 
+from .models import CNAME, BTUTOR, QSUBJECT
+session = session()
 
-from random import randint
-import sys
 
+myHod = User.objects.filter(profile__class_in__exact='HEADS')
 @shared_task
-def add_random_numbers():
-	rand_1 = randint(1,9)
-	rand_2 = randint(1,9)
+def zipped_my_pdfs_(request, model, pk):
+    clss = ['', 'JSS 1', 'JSS 2', 'JSS 3', 'SSS 1', 'SSS 2', 'SSS 3', []]
+    if model == '0':
+        these = CNAME.objects.filter(session__exact=session.profile.session, Class__exact=clss[int(pk)]).order_by('gender', 'full_name')
+        for this in these:
+            this = QSUBJECT.objects.filter(student_name_id__exact=this.id, tutor__term__exact='1st Term', tutor__session__exact=session.profile.session, tutor__Class__exact=this.Class).order_by('tutor__subject')
+            params, term, filename = param_cards(request, this, '0')
+            pdf = Render.render('result/card.html', params, 'pdf/cards /'+pk+'/'+term[-1].split(' ')[0]+'/'+str(this.first().student_name.last_name)+'_'+str(this.first().student_name.first_name)+'.zip', filename)
+            clss[-1].append((filename + ".pdf", pdf))
+    else:
+        tutors = BTUTOR.objects.filter(Class__exact=clss[int(pk)])
+        for tutor in tutors:
+            params, filepath, filename = param_marksheets(request, tutor, myHod)
+            pdf =  Render.render('result/MarkSheetPdf.html', params, filepath+'.zip', filename)
+            clss[-1].append((filename + ".pdf", pdf))
+    return clss
 
-	total = rand_1 + rand_2
-	print total
-	sys.stdout.flush()
-	return total
--- result/views.py
+++ result/views.py
@@ -12,7 +12,7 @@ import os, csv
 from celery.decorators import task 
 #from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
-#from io import BytesIO
+from .tasks import zipped_my_pdfs_
 import requests
 from django.utils import timezone  
 module_dir = os.path.dirname(__file__)  # get current directory
@@ -330,26 +330,8 @@ def zipped_my_pdfs(request, model, pk):
     response['Content-Disposition'] = 'attachment; filename="{}"'.format(clss[int(pk)]+'.zip')
     return response
 
-    
 myHod = User.objects.filter(profile__class_in__exact='HEADS')
-@task(name="generate card/marksheets")
-def zipped_my_pdfs_(request, model, pk):
-    clss = ['', 'JSS 1', 'JSS 2', 'JSS 3', 'SSS 1', 'SSS 2', 'SSS 3', []]
-    if model == '0':
-        these = CNAME.objects.filter(session__exact=session.profile.session, Class__exact=clss[int(pk)]).order_by('gender', 'full_name')
-        for this in these:
-            this = QSUBJECT.objects.filter(student_name_id__exact=this.id, tutor__term__exact='1st Term', tutor__session__exact=session.profile.session, tutor__Class__exact=this.Class).order_by('tutor__subject')
-            params, term, filename = param_cards(request, this, '0')
-            pdf = Render.render('result/card.html', params, 'pdf/cards /'+pk+'/'+term[-1].split(' ')[0]+'/'+str(this.first().student_name.last_name)+'_'+str(this.first().student_name.first_name)+'.zip', filename)
-            clss[-1].append((filename + ".pdf", pdf))
-    else:
-        tutors = BTUTOR.objects.filter(Class__exact=clss[int(pk)])
-        for tutor in tutors:
-            params, filepath, filename = param_marksheets(request, tutor, myHod)
-            pdf =  Render.render('result/MarkSheetPdf.html', params, filepath+'.zip', filename)
-            clss[-1].append((filename + ".pdf", pdf))
-    return clss
-
+    
 #QSUBJECT.objects.filter(tutor__exact=tutor).order_by('student_name__gender', 'student_name__full_name')
 class Pdf(View):#LoginRequiredMixin, 
     def get(self, request, ty, sx, pk):#CARD
